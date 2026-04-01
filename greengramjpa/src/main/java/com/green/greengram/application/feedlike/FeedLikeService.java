81dd29f9f236cf59c506524676a5e4136b1f9e2a
ksy <tndud4615@gmail.com>
2026-04-01 15:31:34 +0900

    [Edit] 좋아요 처리 완료

--- greengramjpa/src/main/java/com/green/greengram/application/feedlike/FeedLikeService.java
+++ greengramjpa/src/main/java/com/green/greengram/application/feedlike/FeedLikeService.java
@@ -1,14 +1,20 @@
 package com.green.greengram.application.feedlike;
 
 import com.green.greengram.application.feedlike.model.FeedLikeReq;
+import com.green.greengram.entity.Feed;
+import com.green.greengram.entity.FeedLike;
+import com.green.greengram.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FeedLikeService {
+    private final FeedLikeRepository feedLikeRepository;
     private final FeedLikeMapper feedLikeMapper;
 
     // 좋아요 처리 true
@@ -17,15 +23,24 @@ public class FeedLikeService {
     // 1경우. select. row가 있다? delete 처리하고 return false
     // 2경우. select. row가 없다? insert 처리하고 return true
     public boolean toggleFeedLike(FeedLikeReq req){
-        // delete를 하고
-        int delAffectedRows = feedLikeMapper.delete(req);
 
-        // if = 1이라면 return false
-        if(delAffectedRows == 1){
-            return false;
+        User user = new User();
+        user.setId( req.getSignedUserId() );
+
+        Feed feed = new Feed();
+        feed.setId( req.getFeedId() );
+
+        FeedLike feedLike = feedLikeRepository.findByFeedAndUser( feed, user );
+
+        // 좋아요 기록이 없으면 좋아요
+        if(feedLike == null){
+            FeedLike newFeedLike = new FeedLike( feed, user );
+            feedLikeRepository.save( newFeedLike );
+            return true;
         }
-        // if = 0이라면 insert 하고 return true
-        feedLikeMapper.save(req);
-        return true;
+        // 이미 좋아요를 했으면 좋아요 취소
+        feedLikeRepository.delete( feedLike );
+        return false;
+
     }
 }
